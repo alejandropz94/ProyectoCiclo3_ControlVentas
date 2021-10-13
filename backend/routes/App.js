399f3a20879aee856a7952c7a21ffd8b06b029f7
enuar <enuaremiliorosales@gmail.com>
2021-10-13 17:26:57 -0500

    pull rama desarrllo

--- backend/routes/App.js
+++ backend/routes/App.js
@@ -14,7 +14,6 @@ app.put("/editProducto", productos.editProducto);
 
 app.delete("/deleteProducto", productos.deleteProducto);
 
-<<<<<<< HEAD
 app.get("/getVentas", ventas.getVentas);
 
 app.post("/addVenta", ventas.addVenta);
@@ -22,11 +21,6 @@ app.post("/addVenta", ventas.addVenta);
 app.put("/editVenta", productos.editVenta);
 
 app.delete("/deleteVenta", ventas.deleteVenta);
-=======
-
-
-
->>>>>>> 7591acf59ececc7d97f66b7d3e65380f185075e1
 
 
 
@@ -39,12 +33,6 @@ app.delete("/deleteVenta", ventas.deleteVenta);
 
 =======
 app.get("/getUsuarios", usuario.getUsuarios);
-
 app.post("/addProducto", usuario.addUsuario);
 
 app.put("/editProducto", usuario.editUsuario);
-
-app.delete("/deleteProducto", usuario.deleteUsuario);
->>>>>>> 7591acf59ececc7d97f66b7d3e65380f185075e1
-
-module.exports = app;
