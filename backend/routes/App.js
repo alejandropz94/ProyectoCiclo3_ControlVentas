0268d1938afb7c7ecbb14d5895d7f79971096ea9
enuar <enuaremiliorosales@gmail.com>
2021-10-13 17:12:33 -0500

    se realiza back ventanas ventas

--- backend/routes/App.js
+++ backend/routes/App.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const productos = require('../controller/Productos');
 
+const ventas = require('../controller/Ventas');
+
 const app = express.Router();
 
 app.get("/getProductos", productos.getProductos);
@@ -10,5 +12,22 @@ app.post("/addProducto", productos.addProducto);
 app.put("/editProducto", productos.editProducto);
 
 app.delete("/deleteProducto", productos.deleteProducto);
+
+app.get("/getVentas", ventas.getVentas);
+
+app.post("/addVenta", ventas.addVenta);
+
+app.put("/editVenta", productos.editVenta);
+
+app.delete("/deleteVenta", ventas.deleteVenta);
+
+
+
+
+
+
+
+
+
 
 module.exports = app;
